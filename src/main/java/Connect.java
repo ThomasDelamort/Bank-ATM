e300be58c3a009413fb58ed15a44e18d7a4dc816
Christian Neal Paredes <nealparedes776@gmail.com>
2026-05-01 20:27:57 +0800

    Add files via upload

---
+++ src/main/java/Connect.java
@@ -0,0 +1,32 @@
+import java.sql.*;
+
+public class Connect {
+    private Connection c;
+    private Statement s;
+
+    public Connect() {
+        String url = "jdbc:mysql://127.0.0.1:3306/bank_login";
+        String user = "root";
+        String password = "Paredes776_677";
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            this.c = DriverManager.getConnection(url, user, password);
+            this.s = c.createStatement();
+            System.out.println("Connected Successfully");
+        } catch (SQLException e) {
+            System.out.println("Failed to connect database: "+e.getMessage());
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public ResultSet execQuery(String q) {
+        ResultSet rsr = null;
+        try {
+            rsr = this.s.executeQuery(q);
+        } catch (SQLException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+        return rsr;
+    }
+}
